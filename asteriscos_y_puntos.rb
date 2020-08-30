1e36906b791ec4a38dbc58ee343ab2e4e5b038ae
Romina Pineda <romi.macka@gmail.com>
2020-08-30 13:47:31 -0400

    ready

--- asteriscos_y_puntos.rb
+++ asteriscos_y_puntos.rb
@@ -3,11 +3,13 @@ n_usuario = ARGV[0].to_i
 if n_usuario < 0
     puts "El número debe ser mayor a 0"
 else
+    i = 1
     while i <= n_usuario
         if i.odd? 
             print "*"
         else
             print "."
         end
+        i += 1
     end
 end
